6f18ab1e782242e0f8892e71a86b3300db93fada
agusnurwanto <agusnurwantomuslim@gmail.com>
2020-10-19 11:09:01 +0700

    initial commit

---
+++ content_scripts.js
@@ -0,0 +1,11 @@
+console.log('run content_script.js');
+
+function injectScript(file, node) {
+    var th = document.getElementsByTagName(node)[0];
+    var s = document.createElement('script');
+    s.setAttribute('type', 'text/javascript');
+    s.setAttribute('src', file);
+    // th.appendChild(s);
+    th.insertBefore(s, th.firstChild);
+}
+injectScript( chrome.extension.getURL('/js/app.js'), 'html');
