3cdf9e64c1f5700cb0c423691c998710b0a1d8da
agusnurwanto <agusnurwantomuslim@gmail.com>
2021-07-27 16:47:20 +0700

    import data RPJM

--- js/content/functions.js
+++ js/content/functions.js
@@ -2796,3 +2796,161 @@ function cek_duplikat_ssh(){
 		}
 	});
 }
+
+function singkron_data_rpjmd_lokal() {
+    if (confirm('Apakah anda yakin melakukan ini? data lama akan diupdate dengan data terbaru.')) {
+        jQuery('#wrap-loading').show();
+        var formDataCustom = new FormData();
+        formDataCustom.append('_token', tokek);
+        formDataCustom.append('v1bnA1m', v1bnA1m);
+        formDataCustom.append('DsK121m', Curut("filter_program=&filter_indi_prog=&filter_skpd="));
+        jQuery.ajax({
+            url: lru1,
+            type: 'post',
+            data: formDataCustom,
+            processData: false,
+            contentType: false,
+            success: function (data) {
+                var data_rpjmd = {
+                    action: 'singkron_data_rpjmd',
+                    tahun_anggaran: config.tahun_anggaran,
+                    api_key: config.api_key,
+                    program: [],
+                    sasaran: [],
+                    tujuan: []
+                };
+                data.data.map(function (program, i) {
+                    data_rpjmd.program[i] = {};
+                    data_rpjmd.program[i].id_misi = program.id_misi;
+                    data_rpjmd.program[i].id_misi_old = program.id_misi_old;
+                    data_rpjmd.program[i].id_program = program.id_program;
+                    data_rpjmd.program[i].id_unik = program.id_unik;
+                    data_rpjmd.program[i].id_unik_indikator = program.id_unik_indikator;
+                    data_rpjmd.program[i].id_unit = program.id_unit;
+                    data_rpjmd.program[i].id_visi = program.id_visi;
+                    data_rpjmd.program[i].indikator = program.indikator;
+                    data_rpjmd.program[i].is_locked = program.is_locked;
+                    data_rpjmd.program[i].is_locked_indikator = program.is_locked_indikator;
+                    data_rpjmd.program[i].kode_sasaran = program.kode_sasaran;
+                    data_rpjmd.program[i].kode_skpd = program.kode_skpd;
+                    data_rpjmd.program[i].kode_tujuan = program.kode_tujuan;
+                    data_rpjmd.program[i].misi_teks = program.misi_teks;
+                    data_rpjmd.program[i].nama_program = program.nama_program;
+                    data_rpjmd.program[i].nama_skpd = program.nama_skpd;
+                    data_rpjmd.program[i].pagu_1 = program.pagu_1;
+                    data_rpjmd.program[i].pagu_2 = program.pagu_2;
+                    data_rpjmd.program[i].pagu_3 = program.pagu_3;
+                    data_rpjmd.program[i].pagu_4 = program.pagu_4;
+                    data_rpjmd.program[i].pagu_5 = program.pagu_5;
+                    data_rpjmd.program[i].program_lock = program.program_lock;
+                    data_rpjmd.program[i].sasaran_lock = program.sasaran_lock;
+                    data_rpjmd.program[i].sasaran_teks = program.sasaran_teks;
+                    data_rpjmd.program[i].satuan = program.satuan;
+                    data_rpjmd.program[i].status = program.status;
+                    data_rpjmd.program[i].target_1 = program.target_1;
+                    data_rpjmd.program[i].target_2 = program.target_2;
+                    data_rpjmd.program[i].target_3 = program.target_3;
+                    data_rpjmd.program[i].target_4 = program.target_4;
+                    data_rpjmd.program[i].target_5 = program.target_5;
+                    data_rpjmd.program[i].target_akhir = program.target_akhir;
+                    data_rpjmd.program[i].target_awal = program.target_awal;
+                    data_rpjmd.program[i].tujuan_lock = program.tujuan_lock;
+                    data_rpjmd.program[i].tujuan_teks = program.tujuan_teks;
+                    data_rpjmd.program[i].urut_misi = program.urut_misi;
+                    data_rpjmd.program[i].urut_sasaran = program.urut_sasaran;
+                    data_rpjmd.program[i].urut_tujuan = program.urut_tujuan;
+                    data_rpjmd.program[i].visi_teks = program.visi_teks;
+                })
+                jQuery.ajax({
+                    url: lru5,
+                    type: 'post',
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                    success: function (data) {
+                        data.data.map(function (sasaran, i) {
+                            data_rpjmd.sasaran[i] = {};
+                            data_rpjmd.sasaran[i].id_misi = sasaran.id_misi;
+                            data_rpjmd.sasaran[i].id_misi_old = sasaran.id_misi_old;
+                            data_rpjmd.sasaran[i].id_sasaran = sasaran.id_sasaran;
+                            data_rpjmd.sasaran[i].id_unik = sasaran.id_unik;
+                            data_rpjmd.sasaran[i].id_unik_indikator = sasaran.id_unik_indikator;
+                            data_rpjmd.sasaran[i].id_visi = sasaran.id_visi;
+                            data_rpjmd.sasaran[i].indikator_teks = sasaran.indikator_teks;
+                            data_rpjmd.sasaran[i].is_locked = sasaran.is_locked;
+                            data_rpjmd.sasaran[i].is_locked_indikator = sasaran.is_locked_indikator;
+                            data_rpjmd.sasaran[i].kode_tujuan = sasaran.kode_tujuan;
+                            data_rpjmd.sasaran[i].misi_teks = sasaran.misi_teks;
+                            data_rpjmd.sasaran[i].sasaran_teks = sasaran.sasaran_teks;
+                            data_rpjmd.sasaran[i].satuan = sasaran.satuan;
+                            data_rpjmd.sasaran[i].status = sasaran.status;
+                            data_rpjmd.sasaran[i].target_1 = sasaran.target_1;
+                            data_rpjmd.sasaran[i].target_2 = sasaran.target_2;
+                            data_rpjmd.sasaran[i].target_3 = sasaran.target_3;
+                            data_rpjmd.sasaran[i].target_4 = sasaran.target_4;
+                            data_rpjmd.sasaran[i].target_5 = sasaran.target_5;
+                            data_rpjmd.sasaran[i].target_akhir = sasaran.target_akhir;
+                            data_rpjmd.sasaran[i].target_awal = sasaran.target_awal;
+                            data_rpjmd.sasaran[i].tujuan_lock = sasaran.tujuan_lock;
+                            data_rpjmd.sasaran[i].tujuan_teks = sasaran.tujuan_teks;
+                            data_rpjmd.sasaran[i].urut_misi = sasaran.urut_misi;
+                            data_rpjmd.sasaran[i].urut_sasaran = sasaran.urut_sasaran;
+                            data_rpjmd.sasaran[i].urut_tujuan = sasaran.urut_tujuan;
+                            data_rpjmd.sasaran[i].visi_teks = sasaran.visi_teks;
+                        });
+                        jQuery.ajax({
+                            url: lru4,
+                            type: 'post',
+                            data: formData,
+                            processData: false,
+                            contentType: false,
+                            success: function (data) {
+                                data.data.map(function (tujuan, i) {
+                                    data_rpjmd.tujuan[i] = {};
+                                    data_rpjmd.tujuan[i].id_misi = tujuan.id_misi;
+                                    data_rpjmd.tujuan[i].id_misi_old = tujuan.id_misi_old;
+                                    data_rpjmd.tujuan[i].id_tujuan = tujuan.id_tujuan;
+                                    data_rpjmd.tujuan[i].id_unik = tujuan.id_unik;
+                                    data_rpjmd.tujuan[i].id_unik_indikator = tujuan.id_unik_indikator;
+                                    data_rpjmd.tujuan[i].id_visi = tujuan.id_visi;
+                                    data_rpjmd.tujuan[i].indikator_teks = tujuan.indikator_teks;
+                                    data_rpjmd.tujuan[i].is_locked = tujuan.is_locked;
+                                    data_rpjmd.tujuan[i].is_locked_indikator = tujuan.is_locked_indikator;
+                                    data_rpjmd.tujuan[i].misi_lock = tujuan.misi_lock;
+                                    data_rpjmd.tujuan[i].misi_teks = tujuan.misi_teks;
+                                    data_rpjmd.tujuan[i].satuan = tujuan.satuan;
+                                    data_rpjmd.tujuan[i].status = tujuan.status;
+                                    data_rpjmd.tujuan[i].target_1 = tujuan.target_1;
+                                    data_rpjmd.tujuan[i].target_2 = tujuan.target_2;
+                                    data_rpjmd.tujuan[i].target_3 = tujuan.target_3;
+                                    data_rpjmd.tujuan[i].target_4 = tujuan.target_4;
+                                    data_rpjmd.tujuan[i].target_5 = tujuan.target_5;
+                                    data_rpjmd.tujuan[i].target_akhir = tujuan.target_akhir;
+                                    data_rpjmd.tujuan[i].target_awal = tujuan.target_awal;
+                                    data_rpjmd.tujuan[i].tujuan_teks = tujuan.tujuan_teks;
+                                    data_rpjmd.tujuan[i].urut_misi = tujuan.urut_misi;
+                                    data_rpjmd.tujuan[i].urut_tujuan = tujuan.urut_tujuan;
+                                    data_rpjmd.tujuan[i].visi_teks = tujuan.visi_teks;
+                                });
+                                var data = {
+                                    message: {
+                                        type: "get-url",
+                                        content: {
+                                            url: config.url_server_lokal,
+                                            type: 'post',
+                                            data: data_rpjmd,
+                                            return: true
+                                        }
+                                    }
+                                };
+                                chrome.runtime.sendMessage(data, function (response) {
+                                    console.log('responeMessage', response);
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+        })
+    }
+}
