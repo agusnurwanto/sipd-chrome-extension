79ad7f10cf9b0014b353104982d9e4e134f47ec6
Agus Nurwanto <agusnurwantomuslim@gmail.com>
2023-11-04 18:11:44 +0700

    tambah kondisi config websql

--- js/background/background.js
+++ js/background/background.js
@@ -190,78 +190,86 @@ chrome.webRequest.onBeforeRequest.addListener(
   ["blocking"]
 );
 
-window.db = openDatabase('SIPD', '1.0', 'sipd database', 50 * 1024 * 1024);
-db.transaction(function (tx) {
-	tx.executeSql('CREATE TABLE IF NOT EXISTS sipd (key, data)', [], function(tx, success){
-		// console.log('success', success);
-	}, function(tx, error){
-		console.log('error', error);
+if(config.websql){
+	window.db = openDatabase('SIPD', '1.0', 'sipd database', 50 * 1024 * 1024);
+	db.transaction(function (tx) {
+		tx.executeSql('CREATE TABLE IF NOT EXISTS sipd (key, data)', [], function(tx, success){
+			// console.log('success', success);
+		}, function(tx, error){
+			console.log('error', error);
+		});
 	});
-});
+}
 
 
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
 	console.log('request & sender', request, sender);
 	var type = request.message.type;
 	if(type == 'actions'){
-		var actions = request.message.content.key;
-		var proses = [];
-		if(actions == 'get_ssh'){
-			var url_ssh = config.sipd_url+"daerah/main/budget/dashboard-komponen/"+config.tahun_anggaran+"/unit/"+config.id_daerah+"/0";
-			chrome.tabs.create({ url: url_ssh });
-			proses = [
-				{ login: 0 },
-				{ get_ssh: 0 }
-			];
-		}else if(actions == 'login_operator'){
-			var id_skpd = '';
-			var url_ssh = config.sipd_url+"daerah/main/budget/dashboard/"+config.tahun_anggaran+"/unit/"+config.id_daerah+"/"+id_skpd;
-			chrome.tabs.create({ url: url_ssh });
-			proses = [
-				{ login: 0 }
-			];
-		}
-		setDB({
-			key: actions,
-			data: {
-				status: 'active',
-				proses: proses
+		if(config.websql){
+			var actions = request.message.content.key;
+			var proses = [];
+			if(actions == 'get_ssh'){
+				var url_ssh = config.sipd_url+"daerah/main/budget/dashboard-komponen/"+config.tahun_anggaran+"/unit/"+config.id_daerah+"/0";
+				chrome.tabs.create({ url: url_ssh });
+				proses = [
+					{ login: 0 },
+					{ get_ssh: 0 }
+				];
+			}else if(actions == 'login_operator'){
+				var id_skpd = '';
+				var url_ssh = config.sipd_url+"daerah/main/budget/dashboard/"+config.tahun_anggaran+"/unit/"+config.id_daerah+"/"+id_skpd;
+				chrome.tabs.create({ url: url_ssh });
+				proses = [
+					{ login: 0 }
+				];
 			}
-		})
-		.then(function(){
-			// getDB({
-			// 	key: actions,
-			// 	debug: true
-			// });
-		});
+			setDB({
+				key: actions,
+				data: {
+					status: 'active',
+					proses: proses
+				}
+			})
+			.then(function(){
+				// getDB({
+				// 	key: actions,
+				// 	debug: true
+				// });
+			});
+		}
 	}else if(type == 'get-actions'){
-		getDB({
-			// debug: true
-		})
-		.then(function(ret){
-			console.log(type, ret);
-			var options = {
-				type: 'response-actions',
-				data: ret,
-				tab: sender.tab
-			};
-			sendMessageTabActive(options);
-		});
+		if(config.websql){
+			getDB({
+				// debug: true
+			})
+			.then(function(ret){
+				console.log(type, ret);
+				var options = {
+					type: 'response-actions',
+					data: ret,
+					tab: sender.tab
+				};
+				sendMessageTabActive(options);
+			});
+		}
 	}else if(type == 'run-actions'){
-		var actions = request.message.content.key;
-		var data = request.message.content.data;
-		var options = {
-			key: actions,
-			data: data
-		};
-		console.log('run-actions', options)
-		setDB(options)
-		.then(function(){
-			// getDB({
-			// 	key: actions,
-			// 	debug: true
-			// });
-		});
+		if(config.websql){
+			var actions = request.message.content.key;
+			var data = request.message.content.data;
+			var options = {
+				key: actions,
+				data: data
+			};
+			console.log('run-actions', options)
+			setDB(options)
+			.then(function(){
+				// getDB({
+				// 	key: actions,
+				// 	debug: true
+				// });
+			});
+		}
 	}else if(type == 'get-url'){
 		relayAjax({
 		    url: request.message.content.url,
